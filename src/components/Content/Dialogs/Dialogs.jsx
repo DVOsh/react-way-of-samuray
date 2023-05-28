0b8f3155b3e4e6629f0d92b0aee106063a435d0c
Danil Oshur <dvosh@internet.ru>
2023-05-28 22:50:59 +0300

    created initial dialog page

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -2,7 +2,39 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
 	return (
-		<div>Dialogs</div>
+		<div className={s.wrapper}>
+			<div className={s.dialogItems}>
+				<div className={`${s.dialogItem} ${s.active}`}>
+					Maks
+				</div>
+				<div className={s.dialogItem}>
+					Aleksey
+				</div>
+				<div className={s.dialogItem}>
+					Kirill
+				</div>
+				<div className={s.dialogItem}>
+					Artem
+				</div>
+				<div className={s.dialogItem}>
+					Sergey
+				</div>
+				<div className={s.dialogItem}>
+					Vlad
+				</div>
+				<div className={s.dialogItem}>
+					Andrew
+				</div>
+				<div className={s.dialogItem}>
+					Nikita
+				</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hello</div>
+				<div className={s.message}>How are you?</div>
+				<div className={s.message}>Bye</div>
+			</div>
+		</div>
 	);
 };
 
