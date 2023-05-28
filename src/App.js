1834393f6b4cc97412935a54d73a969bb3d8995d
Danil Oshur <dvosh@internet.ru>
2023-05-28 20:09:54 +0300

    dialog component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Dialogs from './components/Content/Dialogs/Dialogs';
 import Profile from './components/Content/Profile/Profile';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
@@ -8,7 +9,10 @@ const App = () => {
     <div className='app-wrapper'>
       <Header />
       <Nav />
-      <Profile />
+      <div className='app-wrapper-content'>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div >
   );
 };
