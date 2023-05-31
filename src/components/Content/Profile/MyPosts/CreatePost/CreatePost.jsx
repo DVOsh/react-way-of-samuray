9a89bca8442a3b28a6c53ee3007bb98246ccb5d1
Danil Oshur <dvosh@internet.ru>
2023-05-31 20:46:24 +0300

    refactored and added some styles for profile component

--- src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
+++ src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
@@ -1,11 +1,17 @@
+import s from './CreatePost.module.css';
+
 const CreatePost = () => {
 	return (
-		<div>
-			New post
-			<textarea />
-			<button>Add</button>
+		<div className={s.createPost}>
+			<textarea className={s.input} placeholder="What's new?" />
+			<button
+				className={s.btn}
+				onMouseDown={e => e.target.style.transform = 'scale(.98)'}
+				onMouseUp={e => e.target.style.transform = 'scale(1)'}
+			>New post</button>
 		</div>
 	);
 };
 
 export default CreatePost;
+
--- src/components/Content/Profile/Profile.jsx
+++ src/components/Content/Profile/Profile.jsx
@@ -1,21 +1,12 @@
 import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
 	return (
 		<div>
 			<div className={s.wallpaper}></div>
-			<div className={s.profile}>
-				<div className={s.ava}>
-					avatar
-				</div>
-				<div className={s.profileInfo}>
-					<p>First Name: Hulio</p>
-					<p>Las Name: Padre</p>
-					<p>Age: 32</p>
-					<p>About me: Pedro Padre</p>
-				</div>
-			</div>
+			<ProfileInfo />
 			<MyPosts />
 		</div>
 	);
