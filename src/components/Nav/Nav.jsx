48a798524840345d6713b77e3cc866a87f9a548a
Danil Oshur <dvosh@internet.ru>
2023-05-31 19:50:01 +0300

    added separate components for dialogs, messages and navigation menus

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,24 @@
 import { NavLink } from 'react-router-dom';
 import s from './Nav.module.css';
 
+const NavItem = (props) => {
+	return (
+		<div className={s.item}>
+			<NavLink to={props.path} className={({ isActive }) => isActive ? s.active : undefined}>{props.name}</NavLink>
+		</div>
+	);
+};
+
 const Nav = () => {
 	return (
 		<nav className={s.nav}>
-			<div className={s.item}>
-				<NavLink to='/profile' className={({ isActive }) => isActive ? s.active : undefined}>Profile</NavLink>
-			</div>
-			<div className={s.item}>
-				<NavLink exact="true" to='/dialogs' className={({ isActive }) => isActive ? s.active : undefined}>Messages</NavLink>
-			</div>
-			<div className={s.item}>
-				<NavLink to='/news' className={({ isActive }) => isActive ? s.active : undefined}>News</NavLink>
-			</div>
-			<div className={s.item}>
-				<NavLink to='/music' className={({ isActive }) => isActive ? s.active : undefined}>Music</NavLink>
-			</div>
-			<div className={`${s.item} ${s.set}`}>
-				<NavLink to='/settings' className={({ isActive }) => isActive ? s.active : undefined}>Settings</NavLink>
-			</div>
+
+			<NavItem path='/profile' name="Profile" />
+			<NavItem path='/dialogs' name="Messages" />
+			<NavItem path='/news' name="News" />
+			<NavItem path='/music' name="Music" />
+			<NavItem path='/settings' name="Settings" />
+
 		</nav>
 	);
 };
