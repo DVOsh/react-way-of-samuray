44107c54f55f2ec7a3a03078538ec7b719fc0572
Danil Oshur <dvosh@internet.ru>
2023-06-02 21:10:38 +0300

    removed from the Dialogs component DialogItem and MessageItem Component

---
+++ src/components/Content/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+import s from './DialogItem.module.css';
+
+const DialogItem = (props) => {
+	const path = '/dialogs/' + props.id;
+
+	return (<div className={s.dialogItem}>
+		<NavLink to={path} className={({ isActive }) => isActive ? s.active : undefined}>{props.name}</NavLink>
+	</div>);
+};
+
+export default DialogItem;
