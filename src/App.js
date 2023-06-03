f12b4fba0c159608c7f7765a8a17975301382951
Danil Oshur <dvosh@internet.ru>
2023-06-03 19:10:12 +0300

    removed data from components to index.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ import News from './components/Content/News/News';
 import Music from './components/Content/Music/Music';
 import Settings from './components/Content/Settings/Settings';
 
-const App = () => {
+const App = (props) => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Nav />
       <div className='app-wrapper-content'>
         <Routes>
-          <Route path='/profile' Component={Profile} />
-          <Route path='/dialogs' Component={Dialogs} />
-          <Route path='/news' Component={News} />
-          <Route path='/music' Component={Music} />
-          <Route path='/settings' Component={Settings} />
+          <Route path='/profile' Component={() => <Profile data={props.data.postsData} />} />
+          <Route path='/dialogs' Component={() => <Dialogs data={props.data.dialogsData} />} />
+          <Route path='/news' Component={() => <News />} />
+          <Route path='/music' Component={() => <Music />} />
+          <Route path='/settings' Component={() => <Settings />} />
         </Routes>
       </div>
     </div >
--- src/index.js
+++ src/index.js
@@ -5,11 +5,36 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+const data = {
+  dialogsData:
+  {
+    dialogsMembers: [
+      { id: 1, name: "Maks" },
+      { id: 2, name: "Aleksey" },
+      { id: 3, name: "Kirill" },
+      { id: 4, name: "Artem" },
+      { id: 5, name: "Sergey" },
+      { id: 6, name: "Vlad" },
+      { id: 7, name: "Andrew" },
+      { id: 8, name: "Nikita" },
+    ],
+    messagesData: [
+      { id: 1, message: "Hello" },
+      { id: 2, message: "How are you?" },
+      { id: 3, message: "Bye" },
+    ],
+  },
+  postsData: [
+    { id: 1, message: 'Hello', likesCount: 12 },
+    { id: 2, message: 'Its my first post', likesCount: 11 },
+  ],
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App data={data} />
     </BrowserRouter>
   </React.StrictMode>
 );
