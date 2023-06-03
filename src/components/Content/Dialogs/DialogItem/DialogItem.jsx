a3d44dec18115596d04ebd6b134fd37823ea3cf4
Danil Oshur <dvosh@internet.ru>
2023-06-03 21:38:35 +0300

    created dialogue avatars

--- src/components/Content/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Content/Dialogs/DialogItem/DialogItem.jsx
@@ -5,6 +5,7 @@ const DialogItem = (props) => {
 	const path = '/dialogs/' + props.id;
 
 	return (<div className={s.dialogItem}>
+		<img className={s.dialogItemAvatar} src="https://pixelbox.ru/wp-content/uploads/2021/11/cs-go-avatars-vk-pixelbox.ru-37.jpg" />
 		<NavLink to={path} className={({ isActive }) => isActive ? s.active : undefined}>{props.name}</NavLink>
 	</div>);
 };
