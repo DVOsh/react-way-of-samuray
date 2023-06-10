a1e6df712753433047b6abd8214f41970c478993
Danil Oshur <dvosh@internet.ru>
2023-06-10 21:04:43 +0300

    added handler for New post btn click which outputting textarea message to alert

--- src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
+++ src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import s from './CreatePost.module.css';
 
 const CreatePost = () => {
+	const newPostEl = React.createRef();
+
+	const addPost = () => {
+		const text = newPostEl.current.value;
+		alert(text);
+	}
+
 	return (
 		<div className={s.createPost}>
-			<textarea className={s.input} placeholder="What's new?" />
-			<button
-				className={s.btn}
-				onMouseDown={e => e.target.style.transform = 'scale(.98)'}
-				onMouseUp={e => e.target.style.transform = 'scale(1)'}
-			>New post</button>
+			<textarea className={s.input} placeholder="What's new?" ref={newPostEl} />
+			<button className={s.btn} onClick={addPost}>New post</button>
 		</div>
 	);
 };
