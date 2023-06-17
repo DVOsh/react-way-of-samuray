58a4805dcf29b1fed57a73408503c6511ebcf260
Danil Oshur <dvosh@internet.ru>
2023-06-17 20:22:14 +0300

    added global constants of type of action in store file; added actionCreator functions in store file for create actions for dispatch

--- src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
+++ src/components/Content/Profile/MyPosts/CreatePost/CreatePost.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import s from './CreatePost.module.css';
+import { addPostActionCreator, getPostTextActionCreator, updatePostInputActionCreator } from '../../../../../redux/store';
 
 const CreatePost = props => {
 	const newPostEl = React.createRef();
 
 	const addPost = () => {
-		props.dispatch({ type: 'ADD-POST' });
+		const action = addPostActionCreator();
+		props.dispatch(action);
 	}
 
 	const updateInput = () => {
 		const text = newPostEl.current.value;
-		props.dispatch({ type: 'UPDATE-POST-INPUT', newPostText: text });
+		const action = updatePostInputActionCreator(text);
+		props.dispatch(action);
 	}
+
+	const getPostTextAction = getPostTextActionCreator();
 
 	return (
 		<div className={s.createPost}>
 			<textarea className="input"
 				placeholder="What's new?"
 				ref={newPostEl}
-				value={props.dispatch({ type: 'GET-POST-TEXT' })}
+				value={props.dispatch(getPostTextAction)}
 				onChange={updateInput} />
 			<button className="btn" onClick={addPost}>New post</button>
 		</div>
