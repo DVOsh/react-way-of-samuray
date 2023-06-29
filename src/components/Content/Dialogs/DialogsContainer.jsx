eb22d1d4dc11e898e40b21fe239fe63488adc3a2
Danil Oshur <dvosh@internet.ru>
2023-06-29 20:10:08 +0300

    create store context for the app component and their children

--- src/components/Content/Dialogs/DialogsContainer.jsx
+++ src/components/Content/Dialogs/DialogsContainer.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { sendMessageActionCreator, updateMessageInputActionCreator } from '../../../redux/dialogs-reduser';
 import Dialogs from './Dialogs';
+import StoreContext from '../../../StoreContext';
 
-const DialogsContainer = (props) => {
-	let data = props.store.getState().dialogsData;
+const DialogsContainer = () => {
+	return <StoreContext.Consumer>
+		{
+			store => {
+				let data = store.getState().dialogsData;
 
-	const onSend = () => {
-		const action = sendMessageActionCreator();
-		props.store.dispatch(action);
-	}
+				const onSend = () => {
+					const action = sendMessageActionCreator();
+					store.dispatch(action);
+				}
 
-	const onChangeMessageInput = text => {
-		const action = updateMessageInputActionCreator(text);
-		props.store.dispatch(action);
-	}
+				const onChangeMessageInput = text => {
+					const action = updateMessageInputActionCreator(text);
+					store.dispatch(action);
+				}
 
-	return (<Dialogs
-		onSend={onSend}
-		onChangeMessageInput={onChangeMessageInput}
-		data={data} />);
+				return <Dialogs onSend={onSend} onChangeMessageInput={onChangeMessageInput} data={data} />
+			}
+		}
+	</StoreContext.Consumer>
+
 };
 
 export default DialogsContainer;
