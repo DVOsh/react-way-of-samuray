bef6fcc55317cddfbd6310007e99821ec13ea5b8
Danil Oshur <dvosh@internet.ru>
2023-07-11 20:48:56 +0300

    merge file UsersAPIComponent class component with UsersContainer file

--- src/components/Content/Users/UsersContainer.js
+++ src/components/Content/Users/UsersContainer.js
@@ -1,6 +1,38 @@
 import { connect } from "react-redux";
 import { setCurrentPageAC, setTotalUsersCountAC, setUsersAC, toggleFriendshipAC } from "../../../redux/users-reducer";
-import UsersAPIComponent from "./UsersAPIComponent";
+import axios from 'axios';
+import React from 'react';
+import Users from './Users';
+
+class UsersAPIComponent extends React.Component {
+	componentDidMount() {
+		this.getUsers(this.props.currentPage, this.props.pageSize);
+	}
+
+	getUsers = (currentPage, pageSize) => {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`)
+			.then(response => {
+				this.props.setUsers(response.data.items);
+				this.props.setTotalUsersCount(response.data.totalCount);
+			});
+	}
+
+	setCurrentPage = pageNumber => {
+		this.props.setCurrentPage(pageNumber);
+		this.getUsers(pageNumber, this.props.pageSize);
+	}
+
+	render() {
+		return <Users users={this.props.users}
+			totalUsersCount={this.props.totalUsersCount}
+			pageSize={this.props.pageSize}
+			currentPage={this.props.currentPage}
+			setCurrentPage={this.setCurrentPage}
+			toggleFriendship={this.props.toggleFriendship}
+		/>;
+	}
+}
+
 
 let mapStateToProps = state => {
 	return {
