e8a4fbfcaeef609b35d4eb388b7a987280683c3b
Danil Oshur <dvosh@internet.ru>
2023-07-13 21:03:37 +0300

    send request when profile component did mount; fixed routes of Profile and Dialogs pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import DialogsContainer from './components/Content/Dialogs/DialogsContainer';
 import Music from './components/Content/Music/Music';
 import News from './components/Content/News/News';
-import Profile from './components/Content/Profile/Profile';
 import Settings from './components/Content/Settings/Settings';
 import Header from './components/Header/Header';
 import NavContainer from './components/Nav/NavContainer';
 import UsersContainer from './components/Content/Users/UsersContainer';
+import ProfileContainer from './components/Content/Profile/ProfileContainer';
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
       <NavContainer />
       <div className='app-wrapper-content'>
         <Routes>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/dialogs?/:dialogId' element={<DialogsContainer />} />
+          <Route path='/profile/:userId?' element={<ProfileContainer />} />
+          <Route path='/dialogs/:dialogId?' element={<DialogsContainer />} />
           <Route path='/news' element={<News />} />
           <Route path='/music' element={<Music />} />
           <Route path='/users' element={<UsersContainer />} />
--- src/components/Content/Profile/ProfileContainer.jsx
+++ src/components/Content/Profile/ProfileContainer.jsx
@@ -1,6 +1,16 @@
+import React from 'react';
 import Profile from './Profile';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class ProfileContainer extends React.Component {
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+			.then(response => {
+				// this.props.setUserPage(response.data.totalCount);
+			});
+	}
+
 	render() {
 		return <Profile {...this.props} />
 	}
