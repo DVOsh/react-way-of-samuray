bee3f9c07cc7f7dc3bea1ea4af46607d53e989a9
Danil Oshur <dvosh@internet.ru>
2023-07-14 19:51:40 +0300

    refactoring Profile UI for users data from server

--- src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Content/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,30 +5,23 @@ const ProfileInfo = props => {
 	if (!props.profile)
 		return <Preloader />
 
+
+	let contacts = Object.entries(props.profile.contacts).filter(c => c[1] && c);
+
 	return (
 		<div className={s.profile}>
 			<div className={s.ava}>
-				{/* avatar */}
-				<img src={props.profile.photos.large} alt="Loading" />
+				<img src={props.profile.photos.large} alt="Avatar" />
 			</div>
 			<div className={s.profileInfo}>
-				<p>Name: {props.profile.fullName}</p>
-				<p>About me: {props.profile.aboutMe}</p>
+				<p className={s.userName}>{props.profile.fullName}</p>
+				<i className={s.userStatus}>{props.profile.aboutMe}</i>
 				<div className={s.contacts}>
 					Contacts:
-					<ul>
-						<li>Facebook: {props.profile.contacts.facebook}</li>
-						<li>Website: {props.profile.contacts.website}</li>
-						<li>VK: {props.profile.contacts.vk}</li>
-						<li>Twitter: {props.profile.contacts.twitter}</li>
-						<li>Instagram: {props.profile.contacts.instagram}</li>
-						<li>YouTube: {props.profile.contacts.youtube}</li>
-						<li>GitHub: {props.profile.contacts.github}</li>
-						<li>Main Link: {props.profile.contacts.mainLink}</li>
-					</ul>
+					<ul className={s.contactsList}>{contacts.map((c, i) => <li key={i}>{`${c[0]}: ${c[1]}`}</li>)}</ul>
 				</div>
 				<div className={s.job}>
-					<p>{props.profile.lookingForAJob ? 'V' : 'X'}</p>
+					Ищу работу: {props.profile.lookingForAJob ? <span>&#9989;</span> : <span>&#10062;</span>}
 					<p>{props.profile.lookingForAJobDescription}</p>
 				</div>
 			</div>
